81ad8cb77ba8b668e68cd035f3b1735b4b6c88a5
kiohio707 <fernata707@gmail.com>
2022-08-15 19:31:32 +0700

    Initial Commit

---
+++ app/src/main/java/id/com/example/dicoding_codelab_latihanthemesandstyle/MainActivity.kt
@@ -0,0 +1,11 @@
+package id.com.example.dicoding_codelab_latihanthemesandstyle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
